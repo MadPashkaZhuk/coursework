e04061005be36a5e6473a1a31561d70c1fdbe7d9
MadPashkaZhuk <paskam1note@gmail.com>
2023-11-29 11:19:41 +0300

    added tests and openapi docs

--- hospital/src/main/java/com/zhuk/hospital/controller/DepartmentController.java
+++ hospital/src/main/java/com/zhuk/hospital/controller/DepartmentController.java
@@ -4,11 +4,15 @@ import com.zhuk.hospital.dto.DepartmentDto;
 import com.zhuk.hospital.dto.NewDepartmentDto;
 import com.zhuk.hospital.dto.UserDepartmentAssociationDTO;
 import com.zhuk.hospital.service.DepartmentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -17,29 +21,60 @@ import java.util.Map;
 public class DepartmentController {
     private final DepartmentService departmentService;
     @GetMapping
-    public ResponseEntity<?> findAll() {
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "All info is shown"),
+            @ApiResponse(responseCode = "401", description = "User is not authorized")
+    })
+    public ResponseEntity<List<DepartmentDto>> findAll() {
         return ResponseEntity.ok(departmentService.findAll());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> findById(@PathVariable("id") Long id) {
+    @Operation(summary = "Get department by provided id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Department is found"),
+            @ApiResponse(responseCode = "401", description = "User is not authorized"),
+            @ApiResponse(responseCode = "404", description = "Department with this name doesn't exists")
+    })
+    public ResponseEntity<DepartmentDto> findById(@PathVariable("id") Long id) {
         return ResponseEntity.ok(departmentService.findById(id));
     }
 
     @PatchMapping("/add-user")
+    @Operation(summary = "Add user to department by provided username and departmentt id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "User is added to department"),
+            @ApiResponse(responseCode = "401", description = "User is not authorized"),
+            @ApiResponse(responseCode = "403", description = "Only admin can add user to department"),
+            @ApiResponse(responseCode = "404", description = "User or department doesn't exist")
+    })
     public ResponseEntity<?> addUserToDepartment(@RequestBody UserDepartmentAssociationDTO dto) {
         departmentService.addUserToDepartment(dto);
         return ResponseEntity.noContent().build();
     }
 
     @PatchMapping("/delete-user")
+    @Operation(summary = "delete user from department by provided username and departmentt id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "User is deleted from department"),
+            @ApiResponse(responseCode = "401", description = "User is not authorized"),
+            @ApiResponse(responseCode = "403", description = "Only admin can delete user from department"),
+            @ApiResponse(responseCode = "404", description = "User or department doesn't exist")
+    })
     public ResponseEntity<?> deleteUserFromDepartment(@RequestBody UserDepartmentAssociationDTO dto) {
         departmentService.deleteUserFromDepartment(dto);
         return ResponseEntity.noContent().build();
     }
 
     @PostMapping
-    public ResponseEntity<?> saveDepartment(@RequestBody NewDepartmentDto newDepartmentDto,
+    @Operation(summary = "Save departments with provided dto")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "New department is successfully created"),
+            @ApiResponse(responseCode = "400", description = "Department with same name already exists"),
+            @ApiResponse(responseCode = "401", description = "User is not authorized"),
+            @ApiResponse(responseCode = "403", description = "Only admin can add new users to database")
+    })
+    public ResponseEntity<DepartmentDto> saveDepartment(@RequestBody NewDepartmentDto newDepartmentDto,
                                             UriComponentsBuilder uriComponentsBuilder) {
         DepartmentDto departmentDto = departmentService.save(newDepartmentDto);
         return ResponseEntity.created(uriComponentsBuilder
@@ -49,7 +84,13 @@ public class DepartmentController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<?> updateDepartment(@PathVariable("id") Long id, @RequestBody NewDepartmentDto dto,
+    @Operation(summary = "Update department with provided id and new dto")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Department is successfully updated"),
+            @ApiResponse(responseCode = "401", description = "User is not authorized"),
+            @ApiResponse(responseCode = "403", description = "Only admin can update departments in database")
+    })
+    public ResponseEntity<DepartmentDto> updateDepartment(@PathVariable("id") Long id, @RequestBody NewDepartmentDto dto,
                                               UriComponentsBuilder uriComponentsBuilder) {
         return ResponseEntity.created(uriComponentsBuilder
                         .path("/api/hospital/departments/{id}")
@@ -58,6 +99,12 @@ public class DepartmentController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete department with provided id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Department is successfully deleted"),
+            @ApiResponse(responseCode = "401", description = "User is not authorized"),
+            @ApiResponse(responseCode = "403", description = "Only admin can delete departments from database")
+    })
     public ResponseEntity<?> deleteDepartment(@PathVariable("id") Long id) {
         departmentService.deleteDepartment(id);
         return ResponseEntity.noContent().build();
--- hospital/src/test/java/com/zhuk/hospital/service/UserServiceTest.java
+++ hospital/src/test/java/com/zhuk/hospital/service/UserServiceTest.java
@@ -1,10 +1,11 @@
-package com.zhuk.coursework.service;
+package com.zhuk.hospital.service;
 
-import com.zhuk.coursework.dto.CredentialsDto;
-import com.zhuk.coursework.dto.UserDto;
-import com.zhuk.coursework.enums.UserRoleEnum;
-import com.zhuk.coursework.exception.user.UserAlreadyExistsException;
-import com.zhuk.coursework.exception.user.UserNotFoundException;
+import com.zhuk.hospital.dto.CredentialsDto;
+import com.zhuk.hospital.dto.UserDto;
+import com.zhuk.hospital.entity.DepartmentEntity;
+import com.zhuk.hospital.enums.UserRoleEnum;
+import com.zhuk.hospital.exception.user.UserAlreadyExistsException;
+import com.zhuk.hospital.exception.user.UserNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -20,7 +22,9 @@ import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -40,13 +44,24 @@ public class UserServiceTest {
 
     @BeforeEach
     public void clearTables() throws Exception {
-        try(Connection connection = dataSource.getConnection()) {
-            PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM users");
-            preparedStatement.execute();
+        try (Connection connection = dataSource.getConnection()) {
+            try (PreparedStatement preparedStatementUserDepartment =
+                         connection.prepareStatement("DELETE FROM user_department")) {
+                preparedStatementUserDepartment.execute();
+            }
+            try (PreparedStatement preparedStatementUserDepartment =
+                         connection.prepareStatement("DELETE FROM departments")) {
+                preparedStatementUserDepartment.execute();
+            }
+            try (PreparedStatement preparedStatementUsers = connection.prepareStatement("DELETE FROM users")) {
+                preparedStatementUsers.execute();
+            }
         }
     }
 
+
     @Test
+    @Transactional
     public void findAll_ShouldReturnUserList_WhenDataExists() throws Exception {
         try(Connection connection = dataSource.getConnection()) {
             CredentialsDto credentialsDto = CredentialsDto.builder()
@@ -72,6 +87,7 @@ public class UserServiceTest {
     }
 
     @Test
+    @Transactional
     public void findByUsername_ShouldReturnUser_WhenUserExists() throws Exception {
         try(Connection connection = dataSource.getConnection()) {
             CredentialsDto credentialsDto = CredentialsDto.builder()
@@ -90,8 +106,12 @@ public class UserServiceTest {
                         .username(resultSet.getString("username"))
                         .password(resultSet.getString("password"))
                         .role(UserRoleEnum.valueOf(resultSet.getString("role")))
+                        .departments(new HashSet<>())
                         .build();
-                assertEquals(user, userDto);
+                assertEquals(user.getId(), userDto.getId());
+                assertEquals(user.getUsername(), userDto.getUsername());
+                assertEquals(user.getPassword(), userDto.getPassword());
+                assertEquals(user.getRole(), userDto.getRole());
             }
         }
     }
@@ -174,7 +194,7 @@ public class UserServiceTest {
                     .username("USER")
                     .password("PASS".toCharArray())
                     .build();
-            userService.updateUser("USER", credentialsDto);
+            userService.updateCredentials("USER", credentialsDto);
             String query = "SELECT * FROM users where username = ?";
             PreparedStatement preparedStatement = connection.prepareStatement(query);
             preparedStatement.setString(1, "USER");
@@ -200,7 +220,7 @@ public class UserServiceTest {
                     .password("PWD".toCharArray())
                     .build();
             userService.saveUser(credentialsDto);
-            userService.updateUser("USER", newCredentialsDto);
+            userService.updateCredentials("USER", newCredentialsDto);
             String query = "SELECT * FROM users where username = ?";
             PreparedStatement preparedStatement = connection.prepareStatement(query);
             preparedStatement.setString(1, "NEW");
@@ -209,10 +229,80 @@ public class UserServiceTest {
             while (resultSet.next()) {
                 counter++;
                 assertEquals("NEW", resultSet.getString("username"));
+                System.out.println(resultSet.getString("password"));
                 assertTrue(new BCryptPasswordEncoder().matches("PWD", resultSet.getString("password")));
             }
             assertEquals(1, counter);
         }
     }
 
+    @Test
+    public void addDepartmentByUsername_ShouldAddDepartment_WhenUserExists() throws Exception {
+        try (Connection connection = dataSource.getConnection()) {
+            CredentialsDto credentialsDto = CredentialsDto.builder()
+                    .username("USER")
+                    .password("PASS".toCharArray())
+                    .build();
+            userService.saveUser(credentialsDto);
+            userService.addDepartmentByUsername("USER",
+                    DepartmentEntity.builder()
+                            .name("TEST")
+                            .description("TEST DESCRIPTION")
+                            .build());
+            String query = "SELECT d.id, d.name, d.description FROM users u " +
+                    "JOIN user_department ud ON u.id = ud.user_id " +
+                    "JOIN departments d ON ud.department_id = d.id " +
+                    "WHERE u.username = ?";
+            try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+                preparedStatement.setString(1, "USER");
+                try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                    Set<DepartmentEntity> expectedDepartments = new HashSet<>(Set.of(
+                            DepartmentEntity.builder()
+                                    .name("TEST")
+                                    .description("TEST DESCRIPTION")
+                                    .build()));
+                    Set<DepartmentEntity> actualDepartments = new HashSet<>();
+                    while (resultSet.next()) {
+                        DepartmentEntity department = DepartmentEntity.builder()
+                                .name(resultSet.getString("name"))
+                                .description(resultSet.getString("description"))
+                                .build();
+
+                        actualDepartments.add(department);
+                    }
+                    assertEquals(expectedDepartments, actualDepartments);
+                }
+            }
+        }
+    }
+    @Test
+    public void deleteDepartmentByUsername_ShouldAddDepartment_WhenUserExists() throws Exception {
+        try (Connection connection = dataSource.getConnection()) {
+            CredentialsDto credentialsDto = CredentialsDto.builder()
+                    .username("USER")
+                    .password("PASS".toCharArray())
+                    .build();
+            userService.saveUser(credentialsDto);
+            DepartmentEntity entity = DepartmentEntity.builder()
+                    .name("TEST")
+                    .description("TEST DESCRIPTION")
+                    .build();
+            userService.addDepartmentByUsername("USER", entity);
+            userService.deleteDepartmentByUsername("USER", entity);
+            String query = "SELECT d.id, d.name, d.description FROM users u " +
+                    "JOIN user_department ud ON u.id = ud.user_id " +
+                    "JOIN departments d ON ud.department_id = d.id " +
+                    "WHERE u.username = ?";
+            try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+                preparedStatement.setString(1, "USER");
+                try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                    int counter = 0;
+                    while (resultSet.next()) {
+                        counter++;
+                    }
+                    assertEquals(0, counter);
+                }
+            }
+        }
+    }
 }
